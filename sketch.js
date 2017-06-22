c685ec80984e6bb55fe6d44580e8f1fa050c7a27
Fabien Pannatier <hi@fabienpannatier.ch>
2017-06-22 17:19:05 +0200

    initial commit

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var h = 17;
+
+function setup() {
+  createCanvas(600,600);
+  background(255);
+
+}
+
+function raster() {
+
+  fill(0 ,178,162,90);
+  noStroke();
+  //var h = map(mouseX, 0, width, 0,18);
+
+
+  push();
+  rotate(0);
+  beginShape();
+  vertex (aX[h], aY[h]);
+  vertex (bX[h], bY[h]);
+  vertex (dX[h], dY[h]);
+  vertex (eX[h], eY[h]);
+  vertex (fX[h], fY[h]);
+  vertex (gX[h], gY[h]);
+  vertex (hX[h], hY[h]);
+  vertex (iX[h], iY[h]);
+  vertex (jX[h], jY[h]);
+  vertex (kX[h], kY[h]);
+  vertex (lX[h], lY[h]);
+  vertex (mX[h], mY[h]);
+  vertex (nX[h], nY[h]);
+  vertex (oX[h], oY[h]);
+  vertex (pX[h], pY[h]);
+  endShape(CLOSE);
+  pop();
+
+}
+
+function draw() {
+  background(255);
+  raster();
+
+
+
+}

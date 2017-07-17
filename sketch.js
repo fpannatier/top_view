5f9bc94d478ce8df72000545896340a4b080e0f3
Fabien Pannatier <hi@fabienpannatier.ch>
2017-07-17 14:38:17 +0200

    add functionality
    move center to 0,0
    translate and rotate
    add interface

--- sketch.js
+++ sketch.js
@@ -2,6 +2,15 @@
 
 function setup() {
   createCanvas(600,600);
+  createP('Temperatur').position(30,600);
+  sliderTemp = createSlider(-30,40,5)
+  sliderTemp.position(0,640);
+  createP('Windrichtung').position(263,600);
+  sliderWind = createSlider(0,630,0);
+  sliderWind.position(240,640);
+  createP('Windgeschwindigkeit').position(465,600);
+  sliderSpeed = createSlider(0,17,10);
+  sliderSpeed.position(470,640);
   background(255);
 
 }
@@ -33,14 +42,26 @@ function raster(h) {
   endShape(CLOSE);
   pop();
 
+  //console.log(alti);
+  //console.log(mouseX);
+
 }
 
 function draw() {
+  windDir = sliderWind.value();
+  windSpe = sliderSpeed.value();
+  temp = sliderTemp.value();
+  alti = map(mouseX,0,width,0,17);
+  r = map(temp,-30,40,0,255);
+  b = map(temp,-30,40,255,0);
   background(255);
-
-  fill(0 ,178,162,80);
+  push();
+  translate(300,300);
+  rotate(windDir/100);
+  fill(r,70,b,20);
   noStroke();
-  for(var h=17; h>=0; h--){
+  for(var h=windSpe; h>=0; h--){
     raster(h);
   }
+  pop()
 }
